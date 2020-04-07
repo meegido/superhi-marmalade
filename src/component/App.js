e78a79a9414b6b0ae9be8dfce737d0028b03cb59
Mercedes <meegido@gmail.com>
2020-04-07 12:27:42 +0200

    adding react ruter for navigation between our pages

--- src/component/App.js
+++ src/component/App.js
@@ -1,32 +1,52 @@
 import React from 'react';
+import {  
+  BrowserRouter as Router,  
+  Route
+} from "react-router-dom";
+
 import FeatureMix from './FeatureMix';
 import Header from './Header';
 
+const Home = () => <h1>Home</h1>
+const Archive = () => <h1>Archive</h1>
+const About = () => <h1>About</h1>
+
 function App() {
   return (
-    // this div contains everything
-    <div> 
-      {/* this div contains our page (excluding audioplayer) */}
-      <div className='flex-l justify-end'>
-        {/* FeatureMix (needs styling and updating */}
-        <FeatureMix />
-         <div className='w-50-l relative z-1'>
-          {/* Header */}
-          <Header/>
-          {/* Routed page */}
+    <Router>
+      <div> 
+        {/* this div contains our page (excluding audioplayer) */}
+        <div className='flex-l justify-end'>
+          {/* FeatureMix (needs styling and updating */}
+          <FeatureMix />
+          <div className='w-50-l relative z-1'>
+            {/* Header */}
+            <Header/>
+            {/* Routed page */}
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/archive">
+              <Archive />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+          </div>
+        </div>
+        <div>
+          {/* AudioPlayer */}
+          <iframe 
+            width="100%" 
+            height="60" 
+            src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2Fjuanfalibene%2Fsounds-healing-mixtape-2%2F" 
+            frameBorder="0" 
+            className='player db fixed bottom-0 z-5'>
+          </iframe>
         </div>
       </div>
-      <div>
-        {/* AudioPlayer */}
-        <iframe 
-          width="100%" 
-          height="60" 
-          src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2Fjuanfalibene%2Fsounds-healing-mixtape-2%2F" 
-          frameBorder="0" 
-          className='player db fixed bottom-0 z-5'>
-        </iframe>
-      </div>
-    </div>
+    </Router>
+   
   );
 }
 
