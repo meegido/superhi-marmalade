bbda9254fec4a0ac1d1a78b75462326c99e38e11
Mercedes <meegido@gmail.com>
2020-04-13 12:54:33 +0200

    play mix in a single component with only functionality

--- src/component/Home.js
+++ src/component/Home.js
@@ -5,7 +5,22 @@ const Home = props => <div className='flex flex-wrap justify-between mixes ph3 p
 {/* here we loop thourght all of our mixes */}
   <div className='mix mb4'>
     {/* here we just pass the props straight through */}
-    <Mix name='Women Affair mix' {...props}/>
+    <Mix name='Nitsa Mix' 
+        id='https://api.soundcloud.com/tracks/135147847&auto_play=true' 
+        {...props}
+    />
+  </div>
+  <div className='mix mb4'>
+    <Mix name='Women Affaire' 
+        id='https://api.soundcloud.com/tracks/14960807&auto_play=true' 
+        {...props}
+    />
+  </div>
+  <div className='mix mb4'>
+    <Mix name='Gela Mix' 
+          id='https://api.soundcloud.com/tracks/567572268&auto_play=true' 
+          {...props}
+      />
   </div>
 </div>
 
--- src/component/Mix.js
+++ src/component/Mix.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import PlayButton from './PlayButton';
+import PlayMix from './PlayMix';
 
-const Mix = ({name, playMix, currentMix}) => (
-  <div className='aspect-ratio aspect-ratio--3x4 pointer bg-black' 
-       onClick={() => playMix('https://api.soundcloud.com/tracks/135147847&auto_play=true')}>
-    <div className='ph3 pv4 aspect-ratio--object mix-overlay'>
-      <div className='flex items-center relative z-2'>
-        <h1>Currently playing{currentMix}</h1>
+// here we pick our our name prop, and then the rest of the props we just pass on through
+const Mix = ({name, ...props}) => (
+  <div className='aspect-ratio aspect-ratio--3x4 pointer bg-black'>
+    <PlayMix {...props}>
+      <div className='ph3 pv4 aspect-ratio--object mix-overlay'>
+        <div className='flex items-center relative z-2'>
         <h1 className='f4 f3-l mv0 white ttu biryani pr2 lh-title'>{name}</h1>
         {/* PlayButton goes here */}
         <PlayButton />
+        </div>
       </div>
-    </div>
+    </PlayMix>
+    
   </div>
 )
 
