ee6248e2b62a1ef5affd5ead494878683bdf1caf
Mercedes <meegido@gmail.com>
2020-04-13 19:17:57 +0200

    adding class to show pause button on current mix

--- src/component/PlayMix.js
+++ src/component/PlayMix.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
 // this component wraps around anything that when we click will start playing a mix for us. It provides us functionality rather than any design.
-const PlayMix = ({playMix, id, children}) => (
-  <div className='pointer' onClick={() => playMix(id)}>
+const PlayMix = ({playMix, id, children, currentMix}) => (
+// when our current playing minx equals the id of the mix that this component refers to, we will add a class name of 'playing'
+  <div className={`pointer ${id === currentMix && 'playing'}`} 
+        onClick={() => playMix(id)}>
     {children}
   </div>
 );
